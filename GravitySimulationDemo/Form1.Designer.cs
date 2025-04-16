2fc4909f645677f66ada72800a9cee603c63c314
Atracurium <137397992+CelestialDesolation7@users.noreply.github.com>
2025-04-16 10:43:52 +0800

    初始代码

---
+++ GravitySimulationDemo/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace GravitySimulationDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panelCanvas = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.debugTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelCanvas
+            // 
+            this.panelCanvas.BackColor = System.Drawing.SystemColors.ControlText;
+            this.panelCanvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCanvas.Location = new System.Drawing.Point(0, 0);
+            this.panelCanvas.Name = "panelCanvas";
+            this.panelCanvas.Size = new System.Drawing.Size(1138, 806);
+            this.panelCanvas.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 16;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panelCanvas);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.debugTextBox);
+            this.splitContainer1.Size = new System.Drawing.Size(1689, 806);
+            this.splitContainer1.SplitterDistance = 1138;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // debugTextBox
+            // 
+            this.debugTextBox.AcceptsReturn = true;
+            this.debugTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.debugTextBox.Font = new System.Drawing.Font("黑体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.debugTextBox.Location = new System.Drawing.Point(0, 0);
+            this.debugTextBox.Multiline = true;
+            this.debugTextBox.Name = "debugTextBox";
+            this.debugTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.debugTextBox.Size = new System.Drawing.Size(547, 806);
+            this.debugTextBox.TabIndex = 0;
+            this.debugTextBox.Text = "初始";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1689, 806);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gravity Simulation Demo";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelCanvas;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox debugTextBox;
+    }
+}
+
---
+++ GravitySimulationDemo/Renderer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GravitySimulationDemo
+{
+    public class Renderer
+    {
+        public Renderer()
+        {
+            // 初始化
+        }
+        public void RenderBodies(List<Body> bodies, Graphics graphics)
+        {
+            foreach (var body in bodies)
+            {
+                graphics.FillEllipse(
+                    new SolidBrush(body.DisplayColor),
+                    (float)(body.DisplayPosition.X - body.RenderRadius),
+                    (float)(body.DisplayPosition.Y - body.RenderRadius),
+                    body.RenderRadius * 2,
+                    body.RenderRadius * 2);
+            }
+        }
+        public void RenderTrajectory(List<Body> bodies, Graphics graphics)
+        {
+            // Demo中暂不实现  
+        }
+        public void RenderText(List<Body> bodies, Graphics graphics)
+        {
+            // Demo中暂不实现  
+        }
+
+    }
+}
